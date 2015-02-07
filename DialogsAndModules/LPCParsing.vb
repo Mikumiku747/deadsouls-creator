6e16b4dd321e51f31088d176accb2396d3460833
Daniel Selmes <Mikumiku747@gmail.com>
2015-02-07 19:23:24 +1100

    Started LPC Parsing Functions and Mapping Editor
    These will probably be used later to extract values out of the source
    file we will be editing. There's also a test program where you can. And
    as always, a bit of file shuffling and reaming so it's more clear as to
    what things are.

---
+++ DialogsAndModules/LPCParsing.vb
@@ -0,0 +1,36 @@
+﻿Module LPCParsing
+
+    ''' <summary>
+    ''' Searches through the source string and returns the substring between start and end.
+    ''' </summary>
+    ''' <param name="source">The string to search through</param>
+    ''' <param name="start">The start of your target string</param>
+    ''' <param name="end_">The end of your target string</param>
+    ''' <returns>The substring between start and end_</returns>
+    ''' <remarks>The result doesn't include the start and end strings.</remarks>
+    Public Function GetBetween(source As String, start As String, end_ As String)
+        Dim s_len = start.Length
+        Dim result As String
+        Dim startIndex = source.IndexOf(start) + s_len
+        Dim toBeSearched = source.Substring(startIndex)
+        Dim endIndex = toBeSearched.IndexOf(end_)
+        result = source.Substring(startIndex, endIndex)
+        Return result
+    End Function
+
+    ''' <summary>
+    ''' Searches through a string and replaces whatever is between start and end with a new string
+    ''' </summary>
+    ''' <param name="source">THe string to search through and perform the replacement on.</param>
+    ''' <param name="start">The start of your target area</param>
+    ''' <param name="end_">The end of your target area</param>
+    ''' <param name="new_">The thing to put between them</param>
+    ''' <remarks>The area replaced doesn't include the start and end strings.</remarks>
+    Public Function SetBetween(source As String, start As String, end_ As String, new_ As String)
+        Dim target = GetBetween(source, start, end_)
+        Return source.Replace(start + target + end_, start + new_ + end_)
+    End Function
+
+    '//TODO: Add some more functions so it's super easy to compile the new LPC file and Parse the old one.
+
+End Module
---
+++ Testing Stuff/TextFilterTesting.vb
@@ -0,0 +1,15 @@
+﻿Public Class TextFilterTesting
+
+    Dim filePath As String = "C:\ds\lib\domains\daniel\room\police_box.c"
+
+    Private Sub TextFilterTesting_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim source As String = My.Computer.FileSystem.ReadAllText(filePath)
+        TextBox1.Text = LPCParsing.GetBetween(source, "SetClimate(", ");")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim source As String = My.Computer.FileSystem.ReadAllText(filePath)
+        source = LPCParsing.SetBetween(source, "SetClimate(", ");", TextBox1.Text)
+        MsgBox(source)
+    End Sub
+End Class

192fb355100bee8779c84c3a336a40850fd264e4
danielc5023 <danielc5023@gmail.com>
2015-05-27 00:46:27 +1000

    Template for Beggar Editor
    Ultimate template for beggar making

---
+++ Resources/NPCTemplate.c
@@ -0,0 +1,26 @@
+#include <lib.h>
+
+inherit LIB_SENTIENT;
+
+static void create() {
+	sentient::create();
+	SetKeyName("Blanker");
+	SetId(({ "Blanker" }));
+	SetAdjectives(({ "Blank" }));
+	SetShort("Blank Blanker");
+	SetLong("Blanky Blank BLanker Blankly Blanking");
+	SetCanBite(0);
+	SetWimpy(90);
+	SetPacifist(1);
+	SetInventory(([]));
+	SetLevel(1);
+	SetAutoStand(0);
+	SetRace("human");
+	SetGender("male");
+	SetConsultResponses(([]));
+	SetPosition();
+	SetPolyglot();
+	SetLanguage("common", 100);
+	SetDefaultLanguage("common");
+	SetAction(5, ({}));
+}
